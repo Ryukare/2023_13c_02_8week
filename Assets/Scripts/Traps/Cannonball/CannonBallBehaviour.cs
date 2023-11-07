3520340748461e330c1ce99dbfc1cfb28c6cb7b3
DucAnh8530 <s25395@pjwstk.edu.pl>
2023-11-07 21:01:41 +0100

    VirtualCamera and CannonBall
    Code Repair

--- Assets/Scripts/Traps/Cannonball/CannonBallBehaviour.cs
+++ Assets/Scripts/Traps/Cannonball/CannonBallBehaviour.cs
@@ -40,8 +40,4 @@ public class CannonBallBehaviour : MonoBehaviour
         }
         Destroy(gameObject, _cannonBallConfig.destroyDelay);
     }
-    private void OnBecameInvisible()
-    {
-        Destroy(gameObject);
-    }
 }
