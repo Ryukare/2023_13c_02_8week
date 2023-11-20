afaf9b666120843a04d89e5fd7923a61999deaf9
DucAnh8530 <s25395@pjwstk.edu.pl>
2023-11-20 03:07:40 +0100

    Sprint5
    Cannonball cone radius,
    Start Game View

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -3,44 +3,66 @@ using UnityEditor;
 #endif
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject _mainView;
-    [SerializeField] private GameObject _secondView;
+    [SerializeField] private GameObject _startView;
+    [SerializeField] private GameObject _levelView;
+
+    [SerializeField] private TMP_InputField playerNameInput;
 
     private void Awake()
     {
         _mainView.SetActive(true);
-        _secondView.SetActive(false);
+        _startView.SetActive(false);
+        _levelView.SetActive(false);
     }
     #region Main view
     public void StartClicked()
     {
         _mainView.SetActive(false);
-        _secondView.SetActive(true);
+        _startView.SetActive(true);
     }
 
     public void ExitClicked()
     {
-        #if UNITY_EDITOR
-            EditorApplication.isPlaying = false;
-        #else
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
             Application.Quit();
-        #endif
+#endif
     }
     #endregion
 
-    #region Second view
+    #region Start view
+    public void StartGameClicked()
+    {
+        string playerName = playerNameInput.text;
+        Debug.Log("Player Name: " + playerName);
+
+        _startView.SetActive(false);
+        _levelView.SetActive(true);
+    }
+    public void BackToMainClicked()
+    {
+        _mainView.SetActive(true);
+        _startView.SetActive(false);
+    }
+    #endregion
+
+    #region Level view
     public void LoadLevel(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
 
-    public void BackClicked()
+    public void BacktoStartClicked()
     {
-        _mainView.SetActive(true);
-        _secondView.SetActive(false);
+        _startView.SetActive(true);
+        _levelView.SetActive(false);
     }
     #endregion
 }
